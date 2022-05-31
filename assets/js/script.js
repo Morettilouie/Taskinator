1d0f26004e8c48201aa52e3911f1e5dc943cf662
Louie Moretti <morettilouie@gmail.com>
2022-05-31 17:27:42 -0500

    set tasks to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
             tasks[i].type = taskType;
         }
     }
+    saveTasks();
 
     alert("Task Updated!");
 
@@ -62,8 +63,6 @@ var completeEditTask = function(taskName, taskType, taskId) {
 }
 
 var createTaskEl = function(taskDataObj) {
-    console.log(taskDataObj);
-    console.log(taskDataObj.status);
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -88,6 +87,8 @@ var createTaskEl = function(taskDataObj) {
     taskDataObj.id = taskIdCounter;
 
     tasks.push(taskDataObj);
+
+    saveTasks();
 
     // increase task counter for next unique id
     taskIdCounter++;
@@ -178,6 +179,7 @@ var deleteTask = function(taskId) {
 
     // reassign tasks array to be the same as updatedTaskArr
     tasks = updatedTaskArr;
+    saveTasks();
 }
 
 var taskStatusChangeHandler = function(event) {
@@ -206,7 +208,11 @@ var taskStatusChangeHandler = function(event) {
             tasks[i].status = statusValue;
         }
     }
-    console.log(tasks);
+    saveTasks();
+}
+
+var saveTasks = function() {
+    localStorage.setItem("tasks",JSON.stringify(tasks));
 }
 
 formEl.addEventListener("submit", taskFormHandler);
